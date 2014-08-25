66c1bfa5fe311699702c853bed74eb80e9c65199
Alexey Illarionov <littlesavage@rambler.ru>
2014-08-26 00:02:16 +0400

    Нахер акру

--- app/src/main/java/ru/taaasty/TaaastyApplication.java
+++ app/src/main/java/ru/taaasty/TaaastyApplication.java
@@ -2,24 +2,9 @@ package ru.taaasty;
 
 import android.app.Application;
 
-import org.acra.ACRA;
-import org.acra.ReportingInteractionMode;
-import org.acra.annotation.ReportsCrashes;
-
 import ru.taaasty.utils.NetworkUtils;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
-@ReportsCrashes(
-        formKey = "",
-        formUri = BuildConfig.ACRA_FORM_URI,
-        reportType = org.acra.sender.HttpSender.Type.JSON,
-        httpMethod = org.acra.sender.HttpSender.Method.PUT,
-        formUriBasicAuthLogin = BuildConfig.ACRA_FORM_URI_BASIC_AUTH_LOGIN,
-        formUriBasicAuthPassword = BuildConfig.ACRA_FORM_URI_BASIC_AUTH_PASSWORD,
-        mode = ReportingInteractionMode.TOAST,
-        forceCloseDialogAfterToast = false,
-        resToastText = R.string.crash_toast_text
-)
 public class TaaastyApplication extends Application {
     private static final boolean DBG = BuildConfig.DEBUG;
     private static final String TAG = "TaaastyApplication";
@@ -27,9 +12,6 @@ public class TaaastyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
-        // ACRA мешает при отладке
-        if (BuildConfig.ENABLE_ACRA) ACRA.init(this);
 
         CalligraphyConfig.initDefault("fonts/ProximaNova-Reg.otf", R.attr.fontPath);
 
--- app/src/main/java/ru/taaasty/ui/feeds/TlogActivity.java
+++ app/src/main/java/ru/taaasty/ui/feeds/TlogActivity.java
@@ -1,34 +1,18 @@
 package ru.taaasty.ui.feeds;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v13.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Toast;
-
-import java.util.Locale;
 
 import ru.taaasty.BuildConfig;
 import ru.taaasty.R;
-import ru.taaasty.UserManager;
 import ru.taaasty.model.TlogDesign;
 import ru.taaasty.model.User;
-import ru.taaasty.ui.AdditionalMenuActivity;
 import ru.taaasty.ui.UserInfoActivity;
-import ru.taaasty.ui.feeds.MyAdditionalFeedActivity;
-import ru.taaasty.ui.feeds.MyAdditionalFeedFragment;
-import ru.taaasty.ui.feeds.MyFeedFragment;
-import ru.taaasty.ui.login.LoginActivity;
 import ru.taaasty.widgets.ErrorTextView;
-import ru.taaasty.widgets.Tabbar;
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
 
 
