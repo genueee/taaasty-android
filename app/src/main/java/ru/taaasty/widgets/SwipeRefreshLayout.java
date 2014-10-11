dccf832c0320b401c7a7633de8418ed609ba48ea
Alexey Illarionov <littlesavage@rambler.ru>
2014-10-12 03:33:22 +0400

    Рисуем черный бэкраунд для swipe to refresh во время свайпа

--- app/src/main/java/ru/taaasty/widgets/SwipeRefreshLayout.java
+++ app/src/main/java/ru/taaasty/widgets/SwipeRefreshLayout.java
@@ -1,7 +1,12 @@
 package ru.taaasty.widgets;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.drawable.ShapeDrawable;
 import android.util.AttributeSet;
+import android.view.View;
 
 import ru.taaasty.R;
 
@@ -9,12 +14,19 @@ import ru.taaasty.R;
  * Created by alexey on 14.07.14.
  */
 public class SwipeRefreshLayout extends android.support.v4.widget.SwipeRefreshLayout {
+
+    private final Paint mPreRefreshPaint;
+
     public SwipeRefreshLayout(Context context) {
         this(context, null);
     }
 
     public SwipeRefreshLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
+        mPreRefreshPaint = new Paint();
+        mPreRefreshPaint.setStyle(Paint.Style.FILL);
+        mPreRefreshPaint.setColor(Color.BLACK);
+        ShapeDrawable sh;
     }
 
     @Override
@@ -27,4 +39,18 @@ public class SwipeRefreshLayout extends android.support.v4.widget.SwipeRefreshLa
                 R.color.refresh_widget_color4
         );
     }
+
+    @Override
+    public void draw(Canvas canvas) {
+        View target = getChildAt(0);
+        if (target != null && target.getTop() > 0) {
+            canvas.drawRect(getPaddingLeft(),
+                    getPaddingTop(),
+                    getWidth() - getPaddingLeft() - getPaddingRight(),
+                    target.getTop(),
+                    mPreRefreshPaint);
+        }
+        super.draw(canvas);
+    }
+
 }
