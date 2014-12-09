5d91479a5b3ad6fc2ac5035eae01e3d5ce1aacf8
Alexey Illarionov <littlesavage@rambler.ru>
2014-12-09 19:24:11 +0300

    У первого текстового поста от картинки-шапки надо отступ
    Conflicts:
    	app/src/main/java/ru/taaasty/ui/feeds/ListFeedFragment.java

--- app/src/main/java/ru/taaasty/ui/DividerFeedListInterPost.java
+++ app/src/main/java/ru/taaasty/ui/DividerFeedListInterPost.java
@@ -1,6 +1,7 @@
 package ru.taaasty.ui;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Rect;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
@@ -11,6 +12,8 @@ import ru.taaasty.BuildConfig;
 import ru.taaasty.R;
 import ru.taaasty.adapters.FeedItemAdapter;
 import ru.taaasty.adapters.list.ListEntryBase;
+import ru.taaasty.adapters.list.ListQuoteEntry;
+import ru.taaasty.adapters.list.ListTextEntry;
 
 /**
  * Разделитель между поставми в списке
@@ -21,15 +24,30 @@ public class DividerFeedListInterPost extends RecyclerView.ItemDecoration {
 
     private final int mCommentListPaddingBottom;
 
+    private final int mPaddingAboveTopText;
+
+    private final boolean mAddPaddingAboveTopText;
+
     public DividerFeedListInterPost(Context context, boolean showUserAvatar) {
         super();
+
+        Resources resources = context.getResources();
+
+        mAddPaddingAboveTopText = !showUserAvatar;
         if (showUserAvatar) {
-            mDividerSize = context.getResources().getDimensionPixelSize(R.dimen.feed_list_inter_post_distance_with_avatar);
+            mDividerSize = resources.getDimensionPixelSize(R.dimen.feed_list_inter_post_distance_with_avatar);
         } else {
-            mDividerSize = context.getResources().getDimensionPixelSize(R.dimen.feed_list_inter_post_distance);
+            mDividerSize = resources.getDimensionPixelSize(R.dimen.feed_list_inter_post_distance);
         }
 
         mCommentListPaddingBottom = context.getResources().getDimensionPixelSize(R.dimen.feed_comments_padding_bottom);
+
+        if (mAddPaddingAboveTopText) {
+            mPaddingAboveTopText = resources.getDimensionPixelSize(R.dimen.feed_top_text_padding_bottom);
+        } else {
+            mPaddingAboveTopText = 0;
+        }
+
     }
 
     @Override
@@ -37,6 +55,16 @@ public class DividerFeedListInterPost extends RecyclerView.ItemDecoration {
         boolean initialized = false;
 
         RecyclerView.ViewHolder holder = parent.getChildViewHolder(view);
+
+        // Добавляем отступ над первым текстовым постом, если там текст
+        if (mAddPaddingAboveTopText
+                && holder != null
+                && holder.getPosition() == 1
+                && (holder instanceof ListTextEntry || holder instanceof ListQuoteEntry)) {
+            outRect.set(0, mPaddingAboveTopText, 0, 0);
+            initialized = true;
+        }
+
         if (holder != null && holder.getPosition() > 1) {
             if (holder instanceof  ListEntryBase) {
                 // У всех постов, кроме первого, делаем отступ сверху
